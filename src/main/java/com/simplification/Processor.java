d04aafa959bf8ad213cdeb424a2919614910a250
dsj PC <dsj296631741@163.com>
2022-10-12 00:44:37 +0800

    java code for split files and run JavaParser

--- src/main/java/com/simplification/Processor.java
+++ src/main/java/com/simplification/Processor.java
@@ -8,6 +8,7 @@ import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.TreeVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
 import com.github.javaparser.utils.CodeGenerationUtils;
 import com.github.javaparser.utils.Log;
 import com.github.javaparser.utils.SourceRoot;
@@ -26,8 +27,10 @@ import com.github.javaparser.ast.type.VoidType;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.PackageDeclaration;
 
+import java.io.*;
 import java.nio.file.Paths;
-import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Processor {
     private static final String FILE_PATH = "src/main/resources/Blabla.java";
@@ -58,7 +61,7 @@ public class Processor {
         for (MethodCallExpr node: n.getChildNodesByType(MethodCallExpr.class)){
             count += 1;
             if (count == 1) field = node.getName();
-            System.out.println(node + " : "  + node.getClass().getName());
+//            System.out.println(node + " : "  + node.getClass().getName());
             n.remove(node);
             break;
         }
@@ -125,11 +128,12 @@ public class Processor {
         // System.out.println("==" + n + " || " + n.getParentNode() + " || " + n.findAncestor(FieldAccessExpr.class));
     }
 
-    public static void main(String[] args) {
+    private static void old_main(String[] args){
+
         // JavaParser has a minimal logging class that normally logs nothing.
         // Let's ask it to write to standard out:
         // Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
-        
+
         // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
         // In this case the root directory is found by taking the root from the current Maven module,
         // with src/main/resources appended.
@@ -190,11 +194,219 @@ public class Processor {
                 return super.visit(n, arg);
             }
         }, null);
-        // This saves all the files we just read to an output directory.  
+        // This saves all the files we just read to an output directory.
         sourceRoot.saveAll(
                 // The path of the Maven module/project which contains the Processor class.
                 CodeGenerationUtils.mavenModuleRoot(Processor.class)
                         // appended with a path to "output"
                         .resolve(Paths.get("output")));
+    }
+
+    // assertEquals(a, b) --> class X{ void var24678(){ assertEquals(a, b); } }
+    private static String assertLine2cu(String assertLine){
+
+        String result = "class X { void var24678() { "+assertLine+"; } }";
+        return result;
+    }
+
+    // func(Type, Type){} --> class Y{ void func(Type a, Type b){} }
+    private static  String testMethods2cu(String testMethod){
+        String result = "class Y { void "+ add_parameter2cu(testMethod)+" }";
+        return result;
+    }
+
+    // func(Type, Type) {}  -->  func(Type a, Type b) {}
+    private static String add_parameter2cu(String testMethod){
+        String signature = testMethod.substring(0, testMethod.indexOf("{"));
+        String body = testMethod.substring(testMethod.indexOf("{"));
+//        System.out.println(signature);
+
+
+
+        String method_name = signature.substring(0, signature.indexOf("("));
+        // get type in signature
+        String[] types;
+        try {
+            types = signature.split("\\(")[1].split("\\)")[0].split(",");
+        }catch (Exception e) {
+//            System.out.println("No parameter in method");
+            return testMethod;
+        }
+//        System.out.println(types);
+        String new_signature = method_name + "(";
+        for(int i = 0; i < types.length; i++){
+            if(types[i].trim().equals("")){
+                continue;
+            }
+            new_signature += types[i].trim() + " var24678";
+            if (i!=types.length-1){
+                new_signature += ", ";
+            }
+        }
+        new_signature+=") ";
+        String result = new_signature + body;
+
+        return result;
+    }
+    private static boolean visit_one_pair(String[] code_pair, List<String> output_assertLines, List<String> output_testMethods){
+        try{
+            String assertLine = "";
+            String testMethod = "";
+            for(int i=0;i<2;i++){
+                String code = code_pair[i];
+                if(i==0){
+                    code = assertLine2cu(code);
+                }else if(i==1){
+                    code = code.replaceAll("\"<AssertPlaceHolder>\"",code_pair[0]+";");
+                    code = testMethods2cu(code);
+                }
+                CompilationUnit cu = StaticJavaParser.parse(code);
+
+                cu.accept(new ModifierVisitor<Void>() {
+                    @Override
+                    public Visitable visit(ClassOrInterfaceType n, Void arg) {
+                        processClassOrInterfaceNode(n);
+                        return super.visit(n, arg);
+                    }
+                }, null);
+
+                cu.accept(new ModifierVisitor<Void>() {
+                    @Override
+                    public Visitable visit(FieldAccessExpr n, Void arg) {
+                        processFieldAccessExprNode(n);
+                        return super.visit(n, arg);
+                    }
+                }, null);
+
+                LexicalPreservingPrinter.setup(cu);
+                if(i==0){
+                    assertLine = LexicalPreservingPrinter.print(cu);
+                }else if(i==1){
+                    testMethod = LexicalPreservingPrinter.print(cu);
+                }
+            }
+
+            if(!assertLine.equals("") && !testMethod.equals("")){
+                output_assertLines.add(assertLine);
+                output_testMethods.add(testMethod);
+                return true;
+            }else{
+                return false;
+            }
+
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    private static void new_main(String[] args){
+
+        // JavaParser has a minimal logging class that normally logs nothing.
+        // Let's ask it to write to standard out:
+        // Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
+
+        // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
+        // In this case the root directory is found by taking the root from the current Maven module,
+        // with src/main/resources appended.
+
+        TypeSolver typeSolver = new ReflectionTypeSolver();
+
+        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
+        StaticJavaParser
+                .getConfiguration()
+                .setSymbolResolver(symbolSolver);
+
+
+        List<String> output_assertLines = new ArrayList<>();
+        List<String> output_testMethods = new ArrayList<>();
+
+        String file1 = "input/Testing/assertLines.txt";
+        String file1_output = "output/Testing/assertLines.txt";
+        String file2 = "input/Testing/testMethods.txt";
+        String file2_output = "output/Testing/testMethods.txt";
+
+        File f1 = new File(file1);
+        File f1_output = new File(file1_output);
+        File f2 = new File(file2);
+        File f2_output = new File(file2_output);
+
+        // read lines in f1
+        List<String> lines1 = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f1));
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines1.add(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // read lines in f2
+        List<String> lines2 = new ArrayList<>();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(f2));
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines2.add(line);
+            }
+            br.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        assert lines1.size()==lines2.size();
+        for(int i=0;i<lines1.size();i++){
+            String[] code_pair = new String[2];
+            code_pair[0] = lines1.get(i);
+            code_pair[1] = lines2.get(i);
+            if(visit_one_pair(code_pair, output_assertLines, output_testMethods)){
+                ;
+            }else{
+                System.out.println("Error in processing pair " + i);
+            }
+        }
+
+        // write output_assertLines to file
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f1_output));
+            for(String line: output_assertLines){
+                bw.write(line);
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f2_output));
+            for(String line: output_testMethods){
+                bw.write(line);
+                bw.newLine();
+            }
+            bw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void mytest1(String args[]){
+//        String code = "func(){}";
+        String[] codes = {"func(){}","func(Type){}","func(Type,    Type){}","func  (    Type ,   Type ) { }   ","  func  (    Type ,   Type   ,  Type) { }   "};
+        for(String code :codes){
+            System.out.println(add_parameter2cu(code));
+        }
+    }
+    public static void main(String[] args) {
+        new_main(args);
+        /*
+        忘记testMethods.txt里面应该分成 test method 和 focal method了
+        所以现在跑出来结果不太对. 看起来好像完全没有简化成功.
+         */
+//        mytest1(args);
     }
 }
